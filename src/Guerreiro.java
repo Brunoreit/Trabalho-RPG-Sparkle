c6ba7bb9326b7f786c20b5ce903fbaa05bfa81f2
gabrielbntt <gabriel.fb13@puccampinas.edu.br>
2025-10-29 16:08:56 -0300

    Corrigido classes dos personagens

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -1,6 +1,6 @@
 public class Guerreiro extends Personagem {
 
-    public Guerreiro() throws Exception{
-        super("Guerreiro padrão", 1,1,1,1, new Inventario());
+    public Guerreiro(String nome) throws Exception{
+        super(nome, 1,1,1,1, new Inventario());
     }
 }
