05e7c71bc4873e4aaab8c4c143568b5634119c97
gabrielbntt <gabriel.fb13@puccampinas.edu.br>
2025-10-29 16:18:14 -0300

    Novas modificacoes

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,2 +1,39 @@
 public class Jogo {
+    LeitorArquivo leitor = new LeitorArquivo();
+    private Personagem personagemJogador; // Armazena o personagem escolhido
+
+    public void iniciarJogo() throws Exception {
+        leitor.lerArquivo("src/SparkleInicio.txt");
+
+        // Pede o nome do jogador
+        System.out.print("Digite o nome do seu personagem: ");
+        String nomeJogador = Teclado.getUmString();
+
+        System.out.println("[ 1 ] Guerreiro\n" +
+                "\n" +
+                "[ 2 ] Mago\n" +
+                "\n" +
+                "[ 3 ] Arqueiro\n");
+        System.out.print("\nDigite sua escolha: ");
+        String escolha = Teclado.getUmString();
+
+        switch (escolha) {
+            case "1":
+                System.out.println("\nVocê escolheu ser um Guerreiro!\n");
+                personagemJogador = new Guerreiro(nomeJogador); // Passa o nome
+                break;
+            case "2":
+                System.out.println("\nVocê escolheu ser um Mago!\n");
+                personagemJogador = new Mago(nomeJogador); // Passa o nome
+                break;
+            case "3":
+                System.out.println("\nVocê escolheu ser um Arqueiro\n");
+                personagemJogador = new Arqueiro(nomeJogador); // Passa o nome
+                break;
+        }
+    }
+
+    public Personagem getPersonagemJogador() {
+        return personagemJogador;
+    }
 }
